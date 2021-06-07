54d835e2527561b5557248f8f1965d14f7806143
Laurel <lbentleym1992@gmail.com>
2021-06-07 16:25:01 -0400

    one higher player complete

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -1,10 +1,32 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // Override the Play method to make a Player who always roles one higher than the other player
-    public class OneHigherPlayer
+    public class OneHigherPlayer : Player
     {
+        public override void Play(Player other)
+        {
+            int higherRoll = Roll();
+            int lowerRoll = higherRoll - 1;
 
+            Console.WriteLine($"{Name} rolls a {higherRoll}");
+            Console.WriteLine($"{other.Name} rolls a {lowerRoll}");
+            if (higherRoll > lowerRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (higherRoll < lowerRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
